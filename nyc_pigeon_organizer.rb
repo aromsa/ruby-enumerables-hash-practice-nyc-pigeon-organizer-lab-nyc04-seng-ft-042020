f26a30ee1d5002a09f5e323ff20de771868a34c6
anna bateman <annacbateman@gmail.com>
2020-04-14 18:45:41 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,7 +5,7 @@ def nyc_pigeon_organizer(data)
   data.each do |key, value|
     value.each do |k, v|
       v.each do |name|
-        names_hash = {name => {k => [key]}}
+        names_hash = {k => {name => [key]}}
 
  
     end
